3442151033228e7aff67dce9c75bc71509c61de7
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-03-09 16:36:32 -0300

    componente pageheader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto provident, quibusdam accusamus quidem voluptatem praesentium totam aut ducimus iusto et molestiae in eligendi? Quam, quasi. A ab impedit cupiditate eos.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, qui rem eligendi totam quidem tenetur at maiores accusantium nam voluptatibus consequuntur, ullam possimus in, porro quia ipsam perferendis minima illo?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit ab, earum iste, reprehenderit inventore officiis deleniti nulla nisi iure eveniet, beatae quaerat! Porro voluptas repellendus quam, facere numquam officia.</p>
     </div>
 )
